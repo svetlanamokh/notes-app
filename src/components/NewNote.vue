f814879c470025fd1fd09bf7cc77a5bc04c9750d
Svetlana <mokh-svetlana@yandex.ru>
2021-05-17 20:09:32 +0300

    init commit

---
+++ src/components/NewNote.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="new-note">
+        <label>Title</label>
+        <input v-model="note.title" type="text">
+        <label>Description</label>
+        <textarea v-model="note.descr"></textarea>
+        <div class="exp">
+            <select v-model="note.status" class="priority">
+                <option class="btn btnWhite" value="standart">standart</option>
+                <option class="btn btnYellow" value="important">important</option>
+                <option class="btn btnDanger" value="extremely_important">extremely important</option>
+            </select> 
+            <button class="btn btnPrimary" @click="addNote">New note</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+    props: {
+        note: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        addNote () {
+            this.$emit('addNote', this.note)
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .new-note {
+        text-align: center;
+    }
+
+    .standart {
+        background-color: #fff !important;
+    }
+
+    .important {
+        background-color: rgb(231, 243, 179) !important; 
+    }
+
+    .extremely_important {
+        background-color: rgb(233, 171, 171) !important;
+    }
+
+    .category {
+        margin-right: 40px;
+        background-color: rgb(183, 234, 243);
+        border-radius: 8px;
+        cursor: pointer;
+        padding: 5px 5px;
+    }
+
+    .exp {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+    }
+
+    
+    .priority {
+        border-radius: 30px;
+        background-color: lightblue;
+        margin-top: 20px;
+        padding: 5px 5px;
+        max-width: 260px;
+        color: white;
+        font-size: 25px;
+        text-align: center;
+    }
+
+</style>
